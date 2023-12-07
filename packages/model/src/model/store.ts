9c5473e767e97c3b8fe07145525e004b8dc34368
Timothee <timothee.boussus@gmail.com>
2023-12-07 09:47:41 +0100

    added transaction to loadmany signature

--- packages/model/src/model/store.ts
+++ packages/model/src/model/store.ts
@@ -2,9 +2,9 @@ import { Model, Transaction } from "../index";
 
 export interface Store<M extends Model> {
   save(model: M, transaction?: Transaction): Promise<void>;
-  load(id: M['id'], transaction?: Transaction): Promise<M | undefined>;
-  loadMany(ids: M['id'][]): Promise<M[]>;
+  load(id: M["id"], transaction?: Transaction): Promise<M | undefined>;
+  loadMany(ids: M["id"][], transaction?: Transaction): Promise<M[]>;
   loadAll(transaction?: Transaction): Promise<M[]>;
-  delete(id: M['id'], transaction?: Transaction): Promise<void>;
+  delete(id: M["id"], transaction?: Transaction): Promise<void>;
   streamAll(): AsyncIterable<M>;
 }
