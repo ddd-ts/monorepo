bf618ba39ca6c3c9cbb83ded13aa0b4ab28c69a2
Elies Lou <elieslou.dev@gmail.com>
2023-12-11 14:16:21 +0100

    fix(freeze): fix decorator inference propagation

--- packages/serialization/src/freeze.decorator.ts
+++ packages/serialization/src/freeze.decorator.ts
@@ -1,20 +1,19 @@
 import { Constructor } from "@ddd-ts/traits";
 import { Differences, Equals } from "./differences";
 
-type Unfreezed = { FREEZED: "not_yet" };
-
 type Freezable<T> = {
-	serialize(): T;
+	serialize(...args: any[]): T;
 };
-
-export function Freeze<T = Unfreezed>() {
-	return <C extends Constructor<any>>(
-		target: T extends Unfreezed
-			? C
-			: InstanceType<C> extends Freezable<infer G>
-			  ? Equals<T, G> extends true
-					? C
-					: Differences<T, G>
-			  : never,
-	) => {};
+export function Freeze<T = any>() {
+	return <C>(
+		target: C,
+	): C extends Constructor<Freezable<any>>
+		? InstanceType<C> extends Freezable<infer G>
+			? Equals<T, G> extends true
+				? C
+				: Differences<T, G>
+			: never
+		: never => {
+		return target as any;
+	};
 }
