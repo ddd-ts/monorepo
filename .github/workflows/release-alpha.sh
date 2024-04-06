d02f8db436b68c3a387c4dd24f52b2ef7f1cdf7d
Elies Lou <elieslou.dev@gmail.com>
2024-04-06 16:16:54 +0200

    fix alpha version

--- .github/workflows/release-alpha.sh
+++ .github/workflows/release-alpha.sh
@@ -8,8 +8,8 @@ slugify(){
 branch=`git branch --show-current`
 branch_slug=`slugify $branch`
 
-latest_upstream=`npm view @ddd-ts/model "dist-tags.$branch_slug"`
-latest_upstream="${latest_upstream:=$branch_slug-0}"
+latest_upstream=`npm view @ddd-ts/model "dist-tags.$branch_slug" | awk -F'[.]' '{ print $NF }'`
+latest_upstream="${latest_upstream:="$branch_slug-0"}"
 
 next_version_suffix=`echo $latest_upstream | awk -F'[.-]' '{ print $1 "." $2+1 }'`
 next_version="0.0.0-$next_version_suffix"
