1db53254250e1d52f7266fa9ae38e6d4b8411777
Elies Lou <elieslou.dev@gmail.com>
2024-04-06 12:58:17 +0200

    fix(ci): rework ci jobs

---
+++ .github/workflows/bump-main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+slugify(){
+  echo $1 | iconv -t ascii//TRANSLIT | sed -E -e 's/[^[:alnum:]]+/-/g' -e 's/^-+|-+$//g' | tr '[:upper:]' '[:lower:]'
+}
+
+latest_upstream=`npm view @ddd-ts/model "dist-tags.latest"`
+
+next_version=`echo $latest_upstream | awk -F'[.-]' '{ print $1 "." $2 "." $3+1 }'`
+
+pnpm --filter "@ddd-ts/*" exec npm pkg set version=$next_version
+pnpm --filter "@ddd-ts/*" exec sed -i "s'workspace:\*'$next_version'g" package.json
