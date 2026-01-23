e4714e4766dd40c963770761fd8657ba9ac6831a
Arthur Fontaine <0arthur.fontaine@gmail.com>
2026-01-23 11:34:14 +0100

    refactor(SerializerRegistry): simplify serialize return type

--- packages/core/src/components/serializer-registry.ts
+++ packages/core/src/components/serializer-registry.ts
@@ -92,21 +92,14 @@ export class SerializerRegistry<
     return merged as any;
   }
 
-  serialize<
-    const I extends INamed,
-    const TH extends SerializerRegistry<any, I>,
-  >(
-    this: TH,
-    instance: IsStringLiteral<I["$name"]> extends true ? I : never,
-  ): TH extends SerializerRegistry<infer RRR, any>
-    ? PromiseOr<
-        Pretty<
-          ReturnType<
-            Extract<RRR[number], [INamed<I["$name"]>, any]>[1]["serialize"]
-          >
-        >
-      >
-    : never;
+  serialize(
+    instance: IsStringLiteral<Instances["$name"]> extends true ? Instances : never,
+  ): PromiseOr<
+    {
+      [K in keyof R]: ReturnType<R[K][1]["serialize"]>;
+    }[number]
+  >;
+
   serialize<
     const I extends INamed,
     const TH extends SerializerRegistry<any, I>,
