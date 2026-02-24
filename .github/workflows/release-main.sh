7e88328dc7fe2e33116d75b3fab5cbf1e5251ee5
Arthur Fontaine <0arthur.fontaine@gmail.com>
2026-02-24 11:29:37 +0100

    ci(npm): use pnpm for publishing
    because `npm` does not support `publishConfig.exports` field.....

--- .github/workflows/release-main.sh
+++ .github/workflows/release-main.sh
@@ -14,4 +14,4 @@ next_version=$(echo $latest_upstream | awk -F'[.-]' '{ print $1 "." $2 "." $3+1 
 pnpm --filter "@ddd-ts/*" exec npm pkg set version=$next_version
 pnpm --filter "@ddd-ts/*" exec sed -i "s'workspace:\*'$next_version'g" package.json
 
-npm publish --access public --workspaces
+pnpm publish --access public -r --no-git-checks
